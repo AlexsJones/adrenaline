843a3199c49f8967891e6755abb7bfc832c333f0
Alex Jones <alexsimonjones@gmail.com>
2023-12-11 07:38:45 +0000

    chore: modified example
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

--- examples/server.rs
+++ examples/server.rs
@@ -7,12 +7,8 @@ async fn main() {
 	env_logger::init();
 	let mut ad = Adrenaline::new(Configuration::new_with_local_address("0.0.0.0:8080"));
 	ad.serve(|packet| {
-		let resp_string = string::String::from_utf8_lossy(&packet.bytes);
 		println!("Incoming packet from {}:{}",
 		         packet.remote_address.ip().to_string(), packet.remote_address.port());
-		if resp_string.contains("Test") {
-			return Some("OK".as_bytes().to_vec());
-		}
 		None
 	}).await;
 }
